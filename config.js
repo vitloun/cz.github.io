8ca65d363cd9eb07045f5b3e83cfac7d988e855c
vitloun <loun.vit12@gmail.com>
2024-11-04 12:40:15 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+const API_URL = 'http://localhost/FoodApi/backend/api'
+const IMG_URL = 'http://localhost/FoodApi/backend/api/uploads'
---
+++ main.js
@@ -0,0 +1,3 @@
+// main.js
+console.log(API_URL); // Output: http://localhost/FoodApi/backend/api
+console.log(IMG_URL); // Output: http://localhost/FoodApi/backend/api/uploads
